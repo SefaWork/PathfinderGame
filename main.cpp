df99a39324e86418253937c0641461e3a240dec0
SefaWork <mehmetsefatoksoy@gmail.com>
2025-05-16 15:05:32 +0300

    Listen to poll events

--- main.cpp
+++ main.cpp
@@ -2,14 +2,22 @@
 #include <SDL.h>
 
 int main(int argc, char* argv[]) {
+    bool running = true;
 
+    SDL_Event event;
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
 
-    SDL_Delay(10000);
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
 
     SDL_Quit();
 
